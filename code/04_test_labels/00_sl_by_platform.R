6fb2f43392ee2bdef7ca3b0a71d36e0a223c5d13
E Flynn <erflynn@stanford.edu>
2020-01-24 11:52:22 -0800

    updated accuracy calculations

--- code/04_test_labels/00_sl_by_platform.R
+++ code/04_test_labels/00_sl_by_platform.R
@@ -3,212 +3,165 @@
 require('exprsex')
 require('tidyverse')
 require('data.table')
-
+source("code/utils/assessment_utils.R")
 options(stringsAsFactors = FALSE)
 
-train_rank <- data.table::fread("data/03_silver_std/human/03_out_mat/train_rank.csv", data.table=FALSE)
-train_expr <- data.table::fread("data/03_silver_std/human/03_out_mat/train_expr.csv", data.table=FALSE)
-train_lab <- read.csv("data/03_silver_std/human/03_out_mat/train_lab.csv")
-fgenes_df <- read.csv("data/03_silver_std/human/04_meta_res/fgenes.csv")
-mgenes_df <- read.csv("data/03_silver_std/human/04_meta_res/mgenes.csv")
-
-consensus.genes <- read.csv("data/consensus_genes_human.csv")
-consensus <- consensus.genes %>% mutate(genes=as.character(consensus.genes))
-gse_info <- read_csv("data/01_sample_lists/human_gse_info.csv")
+args <- commandArgs(trailingOnly=TRUE)
+organism <- args[1]
+run_v <- args[2]
 
 
 
-rownames(train_rank) <- consensus$genes
-rownames(train_expr) <- consensus$genes
-
-train_lab <- train_lab %>% mutate("sample"=paste(gse, gsm, sep="."))
-
-train_sex_lab <- train_lab$sex
-names(train_sex_lab) <- train_lab$sample
-
-
-readInTest <- function(idx){
-  # read in the rest of the test data #
-  test_rank <- data.table::fread(sprintf("data/03_silver_std/human/03_out_mat/test_rank_%s.csv", idx), data.table=FALSE)
-  test_lab <- read.csv(sprintf("data/03_silver_std/human/03_out_mat/test_lab_%s.csv", idx)) # HUH. these do not match :(
-  print(dim(test_rank))
-  print(dim(test_lab))
-  rownames(test_rank) <- consensus$genes
-  test_lab <- test_lab %>% mutate("sample"=paste(gse, gsm, sep=".")) # replace GPL
-  test_sex_lab <- test_lab$sex
-  names(test_sex_lab) <- test_lab$sample
-  test_rank <- test_rank[,names(test_sex_lab)]
-  return(list("rank"=test_rank, "lab"=test_sex_lab))
-}
-
-# // TODO look into why test_lab is shorter and fix this
-list.test <- lapply(0:4, readInTest)
-
-fgenes_df2 <- fgenes_df %>% 
-  mutate(id=as.character(entrezgene_id)) %>% 
-  select(id) %>%
-  unique() 
-
-mgenes_df2 <- mgenes_df %>% 
-  mutate(id=as.character(entrezgene_id)) %>% 
-  select(id) %>%
-  unique() 
-
-confMat <- function(actual, pred){
-  table(data.frame(cbind(actual, pred)))
+# ---- prepare data --- #
+common.genes <- read.csv(sprintf("data/consensus_genes_%s.csv", organism))$consensus.genes
+if (organism=="human"){
+  full.genes <- read.csv(sprintf("data/consensus_genes_%s_full.csv", organism))$consensus.genes
+  genes <- full.genes
+} else {
+  genes <- common.genes
 }
 
 
-calcAcc <- function(actual, pred){
-  sum(pred==actual)/length(actual)
-}
+train_rank <- read_process_mat(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_rank.csv", organism, run_v), genes)
+train_expr <- read_process_mat(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_expr.csv", organism, run_v), genes)
+train_lab <- read_process_lab(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_lab.csv", organism, run_v))
 
-fit <- exprsex::trainSexLab(as.matrix(train_rank),
-                     train_sex_lab,
-                     female_genes=fgenes_df2$id,
-                     male_genes=mgenes_df2$id)
-save(fit, file="data/fit_0120.RData")
-pred_train <- predSexLab(fit, as.matrix(train_rank))
-pred_test0 <- predSexLab(fit, as.matrix(list.test[[0]]$rank))
-
-confMat(train_sex_lab, pred_train)
-confMat(list.test[[0]]$lab, pred_test0)
-
-calcAcc(train_sex_lab, pred_train)
-calcAcc(list.test[[0]]$lab, pred_test0)
-
-pred_train_df <- predSexLab(fit, as.matrix(train_rank), scores=TRUE)
-pred_train2 <- data.frame(t(pred_train_df))
-pred_train2$true_sex <- train_sex_lab
-ggplot(pred_train2, aes(x=score_f, y=score_m))+geom_point(aes(color=true_sex, alpha=0.3))+
-  geom_abline(slope=1, intercept=fit$threshold) # TODO - add platform here
-# this does not look good
-#score_m  = fit$threshold + score_f
-
-## BACKGROUND DATA ON PLATFORM ##
-train_gse <- gse_info %>% 
-  filter(gse %in% unique(str_replace_all(train_lab$gse, "\\.", "-"))) 
-
-pred_train2$gsm <- sapply(rownames(pred_train2 ), function(x) {l <- strsplit(x, "\\.")[[1]]; l[[length(l)]]})
-pred_train2$gse <- sapply(rownames(pred_train2 ), function(x) {
-  z <- strsplit(x, "\\.")[[1]]; ifelse(length(z)==2, z[[1]], paste(z[[1]], z[[2]], sep="-"))})
-
-pred_train_gpl <- left_join(pred_train2, train_gse %>% select(gse, gpl))
-
-## we see that much of this *IS* separable using the score metric, 
-##    but there is large platform-to-platform variability
-ggplot(pred_train_gpl, aes(x=score_f, y=score_m))+geom_point(aes(color=true_sex, alpha=0.3))+
-  geom_abline(slope=1, intercept=fit$threshold)+facet_wrap(vars(gpl))
+sl_genes <- read_clean_sl_genes(organism, run_v)
 
 
+# ---- train and save a model ---- #
+fit <- exprsex::trainSexLab(train_rank,
+                            train_lab,
+                            female_genes=sl_genes$f,
+                            male_genes=sl_genes$m)
+save(fit, file=sprintf("data/04_fits/fit_%s_%s.RData", organism, run_v))
+pred_train <- predSexLab(fit, train_rank)
 
-# LOOK AT THE TEST DATA
-acc_test <- lapply(list.test, function(x) {preds <- predSexLab(fit, as.matrix(x$rank));
-calcAcc(x$lab, preds)})
-
-pred_test_df <- predSexLab(fit, as.matrix(list.test[[4]]$rank), scores=TRUE)
-pred_test <- data.frame(t(pred_test_df))
-pred_test$true_sex <- list.test[[4]]$lab
-ggplot(pred_test, aes(x=score_f, y=score_m))+
-  geom_point(aes(color=true_sex), alpha=0.5)+ 
-  geom_abline(slope=1, intercept=fit$threshold)
-
-
-getGSEsDotted <- function(list.names){
-  # this deals with the fact that many inputted are dotted
-  ##samples <- sapply(list.names, function(x) {l <- strsplit(x, "\\.")[[1]]; l[[length(l)]]})
-  studies <- sapply(list.names, function(x) {
-    z <- strsplit(x, "\\.")[[1]]; ifelse(length(z)==2, z[[1]], paste(z[[1]], z[[2]], sep="-"))})
-  return(studies)
-}
+confMat(train_lab, pred_train)
+calcAcc(train_lab, pred_train) # 93.9% training accuracy for common
 
 
-byPlatAcc <- function(fit, df, lab, my.gpl){
-  gses <- unique(sapply((gse_info %>% filter(gpl == my.gpl))$gse, function(x) strsplit(x, "\\.")[[1]][[1]]))
-  to.keep <- sapply(colnames(df), function(x) ifelse(strsplit(x, "\\.")[[1]][[1]] %in% gses, TRUE, FALSE))
-  # get the overlap in gses2
-  df.gpl <- df[,to.keep]
-  gses2 <- unique(lapply(colnames(df.gpl), function(x) strsplit(x, "\\.")[[1]][[1]]))
-  preds <- predSexLab(fit, as.matrix(df.gpl))
-  acc <- calcAcc(preds, lab[to.keep])
-  return(data.frame("gpl"=my.gpl, "acc"=acc, "num_studies"=length(gses2), "num_samples"=length(preds)))
-}
+# --- grab gpl info --- #
+silver_std <- read.csv(sprintf("data/01_sample_lists/gse_for_silver_std_%s.csv", organism), stringsAsFactors = FALSE)
+silver_std_s <- silver_std %>% filter(!str_detect(gpl, "\\|")) 
+silver_std_m <- silver_std %>% filter(str_detect(gpl, "\\|"))  %>% separate_rows(gpl, sep="\\|") %>%
+  mutate(gse=sprintf("%s-%s", gse, gpl)) 
+silver_std2 <- data.frame(rbind(silver_std_s, silver_std_m))
+silver_std2 %>% write_csv(sprintf("data/01_sample_lists/silver_std_%s_reform.csv", organism))
 
-all_plats_res <- function(fit, df, lab){
-  gses <- unique(getGSEsDotted(colnames(df)))
-  gpl_lst <- unique((gse_info %>% filter(gse %in% gses))$gpl)
-  do.call(rbind, lapply(gpl_lst, function(my.gpl) byPlatAcc(fit, df, lab, my.gpl)))
-}
-train_acc <- all_plats_res(fit, train_rank, train_sex_lab) %>% arrange(acc)
+train_pred_df <- pred_out_df(fit, train_rank, train_lab, organism)
+summarizeAcc(train_pred_df, "train", organism)
 
-# ---- we need to group the test data by GPL ---- #
-# there are only eight --> not so bad
+# ---  test the model --- #
+#  - compute the full test accuracy
+#  - compute the by-platform test accuracy
+# // TODO: - compute the by-study test accuracy --> plot the accuracy by platform across all
 
-# get a list of test gpls
-test.gses <- unique(unlist(lapply(list.test, function(x) getGSEsDotted(names(x$lab)))))
-test.info <-  gse_info %>% filter(gse %in% test.gses) 
-test.gpls <- (test.info %>% select(gpl) %>% unique())$gpl
-
-
-by_plat_test <- function(list.dat, my.gpl){
-  gse_gpl <- test.info %>% select(gse, gpl) %>% filter(gpl==my.gpl) %>% select(gse) %>% unique()
-  list.dat2 <- lapply(list.dat, function(dat){
-    lab <- dat$lab; 
-    df <- dat$rank;
-    gses.dat <- getGSEsDotted(names(lab))
-    to.keep <- c(gses.dat %in% gse_gpl$gse)
-    lab2 <- lab[to.keep]
-    df2 <- df[,to.keep]
-    return(list("lab"=lab2, "rank"=df2))
-  })
-  rank2 <- do.call(cbind, lapply(list.dat2, function(x) x$rank))
-  lab2 <- unlist(lapply(list.dat2, function(x) x$lab))
-  return(list("rank"=rank2, "lab"=lab2))
-}
-
-lapply(test.gpls, function(my.gpl){
-  test_gpl <- by_plat_test(list.test, my.gpl)
-  save(test_gpl, file=sprintf("data/03_silver_std/human/03_out_mat/test_%s.RData", tolower(my.gpl)))
+if (organism == "human"){
+  # // TODO look into why test_lab is shorter and fix this
+  list.test_c <- lapply(0:2, function(i) readInTest(organism, i, "common", common.genes)) 
+  list.test_f <- lapply(0:1, function(i) readInTest(organism, i, "full", full.genes))
+  list.test_r <- lapply(0:2, function(i) readInTest(organism, i, "rare", full.genes))
+  test_c_pred <- do.call(rbind, lapply(1:3, function(i) 
+                           pred_out_df(fit, list.test_c[[i]]$rank, list.test_c[[i]]$lab, organism)))
   
-})
-
-# by platform-accuracy in the test data
-test_plat_acc <- function(my.gpl){
-  print(my.gpl)
-  miceadds::load.Rdata(sprintf("data/03_silver_std/human/03_out_mat/test_%s.RData", tolower(my.gpl)), "test_gpl")
-  preds_test_df <- predSexLab(fit, as.matrix(test_gpl$rank), scores=TRUE)
-  num_samples <- length(test_gpl$lab)
-  num_studies <- length(unique(sapply(names(test_gpl$lab), function(x) strsplit(x, "\\.")[[1]][[1]])))
+  test_f_pred <- do.call(rbind, lapply(1:2, function(i) 
+                           pred_out_df(fit, list.test_f[[i]]$rank, list.test_f[[i]]$lab, organism)))
   
-  pred_test <- data.frame(t(pred_test_df))
-  pred_test$true_sex <- test_gpl$lab
-  ggplot(pred_test, aes(x=score_f, y=score_m))+
-    geom_point(aes(color=true_sex), alpha=0.5)+ 
-    geom_abline(slope=1, intercept=fit$threshold)+
-    ggtitle(sprintf("%s (test)", my.gpl))
-  ggsave(file=sprintf("test_score_%s.png", tolower(my.gpl)), width=5, height=4)
-  acc_gpl <- calcAcc(pred_test$sex, test_gpl$lab)
+  test_r_pred <- do.call(rbind, lapply(1:3, function(i) 
+                           pred_out_df(fit, list.test_r[[i]]$rank, list.test_r[[i]]$lab, organism)))
   
-  return(data.frame("gpl"=my.gpl, "acc"=acc_gpl, "num_studies"=num_studies, "num_samples"=num_samples))
-}
+  train_common <- read_process_mat(sprintf("data/03_silver_std/%s/03_out_mat/train_common_rank.csv", organism), common.genes)
+  train_c_lab <- read_process_lab(sprintf("data/03_silver_std/%s/03_out_mat/train_common_lab.csv", organism))
+  train_c_pred <- pred_out_df(fit, train_common, train_c_lab, organism)
+  
+  summarizeAcc(train_c_pred, "train_common", organism)
+  summarizeAcc(test_c_pred, "test_common", organism)
+  summarizeAcc(rbind(test_c_pred, test_f_pred), "test_full", organism)
+  summarizeAcc(test_f_pred, "test_full_sm", organism)
+  summarizeAcc(test_r_pred, "test_rare", organism)
+} else{
+  test_c <-  readInTest(organism, 0, "common", common.genes)
+  test_c_pred <- pred_out_df(fit, test_c$rank, test_c$lab, organism)
+  summarizeAcc(test_c_pred, "test", organism)
+  
+  if (organism == "mouse"){
+    test_r <-  readInTest(organism, 0, "rare", common.genes)
+    test_r_pred <- pred_out_df(fit, test_r$rank, test_r$lab, organism)
+    summarizeAcc(test_r_pred, "test_rare", organism)
+    
+  }
+} 
 
-# plot for test data
-
-test_acc <- lapply(test.gpls, test_plat_acc)
-test_acc_df <- do.call(rbind, test_acc)
-
-#### TODO - save the results
-
-train_acc %>% filter(gpl %in% test.gpls) %>% summarize(ntot=sum(num_studies), nsamp=sum(num_samples))
-# 13 studies, 808 samples, 8 platforms
-
-train_acc %>% filter(!gpl %in% test.gpls) %>% arrange(desc(num_studies)) 
-# 3 of these have 2 -- so we *COULD* test on better
-# but what about the rest??
 
 
-## // TODO - this does not work because of NAs?? or b/c we need to adjust??
-#fit_expr <- exprsex::trainSexLab(as.matrix(train_expr),
-#                            train_sex_lab,
-#                            female_genes=fgenes_df2$id,
-#                            male_genes=mgenes_df2$id)
+
+
+# ------ scratch work ----- #
+
+# # get a list of test gpls
+# test.gses <- unique(unlist(lapply(list.test, function(x) getGSEsDotted(names(x$lab)))))
+# test.info <-  gse_info %>% filter(gse %in% test.gses) 
+# test.gpls <- (test.info %>% select(gpl) %>% unique())$gpl
+# 
+# 
+# by_plat_test <- function(list.dat, my.gpl){
+#   gse_gpl <- test.info %>% select(gse, gpl) %>% filter(gpl==my.gpl) %>% select(gse) %>% unique()
+#   list.dat2 <- lapply(list.dat, function(dat){
+#     lab <- dat$lab; 
+#     df <- dat$rank;
+#     gses.dat <- getGSEsDotted(names(lab))
+#     to.keep <- c(gses.dat %in% gse_gpl$gse)
+#     lab2 <- lab[to.keep]
+#     df2 <- df[,to.keep]
+#     return(list("lab"=lab2, "rank"=df2))
+#   })
+#   rank2 <- do.call(cbind, lapply(list.dat2, function(x) x$rank))
+#   lab2 <- unlist(lapply(list.dat2, function(x) x$lab))
+#   return(list("rank"=rank2, "lab"=lab2))
+# }
+# 
+# lapply(test.gpls, function(my.gpl){
+#   test_gpl <- by_plat_test(list.test, my.gpl)
+#   save(test_gpl, file=sprintf("data/03_silver_std/human/03_out_mat/test_%s.RData", tolower(my.gpl)))
+#   
+# })
+# 
+# # by platform-accuracy in the test data
+# test_plat_acc <- function(my.gpl){
+#   print(my.gpl)
+#   miceadds::load.Rdata(sprintf("data/03_silver_std/human/03_out_mat/test_%s.RData", tolower(my.gpl)), "test_gpl")
+#   preds_test_df <- predSexLab(fit, as.matrix(test_gpl$rank), scores=TRUE)
+#   num_samples <- length(test_gpl$lab)
+#   num_studies <- length(unique(sapply(names(test_gpl$lab), function(x) strsplit(x, "\\.")[[1]][[1]])))
+#   
+#   pred_test <- data.frame(t(pred_test_df))
+#   pred_test$true_sex <- test_gpl$lab
+#   ggplot(pred_test, aes(x=score_f, y=score_m))+
+#     geom_point(aes(color=true_sex), alpha=0.5)+ 
+#     geom_abline(slope=1, intercept=fit$threshold)+
+#     ggtitle(sprintf("%s (test)", my.gpl))
+#   ggsave(file=sprintf("test_score_%s.png", tolower(my.gpl)), width=5, height=4)
+#   acc_gpl <- calcAcc(pred_test$sex, test_gpl$lab)
+#   
+#   return(data.frame("gpl"=my.gpl, "acc"=acc_gpl, "num_studies"=num_studies, "num_samples"=num_samples))
+# }
+# 
+# # plot for test data
+# 
+# test_acc <- lapply(test.gpls, test_plat_acc)
+# test_acc_df <- do.call(rbind, test_acc)
+# 
+# train_acc %>% filter(gpl %in% test.gpls) %>% summarize(ntot=sum(num_studies), nsamp=sum(num_samples))
+# # 13 studies, 808 samples, 8 platforms
+# 
+# train_acc %>% filter(!gpl %in% test.gpls) %>% arrange(desc(num_studies)) 
+# # 3 of these have 2 -- so we *COULD* test on better
+# # but what about the rest??
+# 
+# 
+# ## // TODO - this does not work because of NAs?? or b/c we need to adjust??
+# #fit_expr <- exprsex::trainSexLab(as.matrix(train_expr),
+# #                            train_sex_lab,
+# #                            female_genes=fgenes_df2$id,
+# #                            male_genes=mgenes_df2$id)
--- code/04_test_labels/02_missingness_accuracy.R
+++ code/04_test_labels/02_missingness_accuracy.R
@@ -3,94 +3,103 @@
 # look at the the training data from the test platforms
 
 
+
 require('exprsex')
 require('tidyverse')
 require('data.table')
-
+source("code/utils/assessment_utils.R")
 
 # <--- READ IN THE INPUT ---> #
 options(stringsAsFactors = FALSE)
 
-train_rank <- data.table::fread("data/03_silver_std/human/03_out_mat/train_rank.csv", data.table=FALSE)
-train_lab <- read.csv("data/03_silver_std/human/03_out_mat/train_lab.csv")
-train_lab <- train_lab %>% mutate(gse=str_replace_all(gse, "\\.", "-"))
-fgenes_df <- read.csv("data/03_silver_std/human/04_meta_res/fgenes.csv")
-mgenes_df <- read.csv("data/03_silver_std/human/04_meta_res/mgenes.csv")
+# ---- prepare data --- #
+common.genes <- read.csv(sprintf("data/consensus_genes_%s.csv", organism))$consensus.genes
+if (organism=="human"){
+  full.genes <- read.csv(sprintf("data/consensus_genes_%s_full.csv", organism))$consensus.genes
+  genes <- full.genes
+} else {
+  genes <- common.genes
+}
 
-fgenes_df2 <- fgenes_df %>% 
-  mutate(id=as.character(entrezgene_id)) %>% 
-  select(id) %>%
-  unique() 
 
-mgenes_df2 <- mgenes_df %>% 
-  mutate(id=as.character(entrezgene_id)) %>% 
-  select(id) %>%
-  unique()
-sl_genes <- c(fgenes_df2$id, mgenes_df2$id) 
+train_rank <- read_process_mat(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_rank.csv", organism, run_v), genes)
+train_expr <- read_process_mat(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_expr.csv", organism, run_v), genes)
+train_lab <- read_process_lab(sprintf("data/03_silver_std/%s/03_out_mat/train_%s_lab.csv", organism, run_v))
 
-consensus.genes <- read.csv("data/consensus_genes_human.csv")
-consensus <- consensus.genes %>% mutate(genes=as.character(consensus.genes))
-rownames(train_rank) <- consensus$genes
-train_lab <- train_lab %>% mutate("sample"=paste(gse, gsm, sep="."))
-train_sex_lab <- train_lab$sex
-names(train_sex_lab) <- train_lab$sample
 
-gse_info <- read_csv("data/01_sample_lists/human_gse_info.csv")
 
-# todo: hard-coded
-test.gpls <- c("GPL17586", "GPL10558", "GPL17077", "GPL6480","GPL6102", "GPL4133", "GPL6884", "GPL14550")
-
-gse_gpl2 <- gse_info %>% 
-  filter(gpl %in% test.gpls) %>% 
-  select(gse, gpl) %>% unique()
-
-keep.dat <- c(train_lab$gse %in% gse_gpl2$gse)
-train_rank2 <- train_rank[,keep.dat]
-train_sex_lab2 <- train_sex_lab[keep.dat]
-save(train_rank2, train_sex_lab2, file="data/03_silver_std/human/03_out_mat/train_plat_filt.RData")
-fit2 <- exprsex::trainSexLab(as.matrix(train_rank2),
-                            train_sex_lab2,
-                            female_genes=fgenes_df2$id,
-                            male_genes=mgenes_df2$id)
-pred_train2 <- predSexLab(fit2, as.matrix(train_rank2))
-
-confMat(train_sex_lab2, pred_train2)
-calcAcc(train_sex_lab2, pred_train2) # 95.1%
+sl_genes <- read_clean_sl_genes(organism, run_v)
 
 # ok now we want the counts of missingness by column
-train_sl <- train_rank2[sl_genes,] # there are 30 total
+sl_overlap <-intersect(c(sl_genes$f, sl_genes$m), rownames(train_rank))
+train_sl <- train_rank[sl_overlap,] # there are 31 total
 nas.by.col <- apply(train_sl, 2, function(x) sum(is.na(x)))
 summary(nas.by.col)
-table(nas.by.col==1) # 275 <-- we can try these!
-preds.1na <- predSexLab(fit2, as.matrix(train_sl[,c(nas.by.col==1)]))
-calcAcc(train_sex_lab2[c(nas.by.col==1)], preds.1na) # 97.1%
+table(nas.by.col<=1) # 4510 <-- we can try these!
+load(file=sprintf("data/04_fits/fit_%s_%s.RData", organism, run_v)) # full
+preds.1na <- predSexLab(fit, as.matrix(train_sl[,c(nas.by.col<=1)]))
+calcAcc(train_lab[c(nas.by.col<=1)], preds.1na) # 96.9%
 
-dat <- train_sl[,c(nas.by.col==1)]
-test_labels <- train_sex_lab2[c(nas.by.col==1)]
+# missingness by platform??
+
+dat <- train_sl[,c(nas.by.col<=1)]
+test_labels <- train_lab[c(nas.by.col<=1)]
 
 set.seed(116)
 removeGenes <-function(nGenes, dat, test_labels) {
-  to.remove <- sl_genes[sample(1:length(sl_genes), nGenes, replace=FALSE)]
+  to.remove <- sl_overlap[sample(1:length(sl_overlap), nGenes, replace=FALSE)]
   dat[to.remove,] <- NA
-  acc <- calcAcc(test_labels, predSexLab(fit2, as.matrix(dat)))
+  acc <- calcAcc(test_labels, predSexLab(fit, as.matrix(dat)))
   return(data.frame("ngenes"=nGenes, "acc"=acc))
 }
 
 ## // TODO -- do this with the testing data
 acc_remove <- do.call(rbind, lapply(1:50, function(i)
                      do.call(rbind, 
-                      lapply(1:30, function(x) removeGenes(x, dat, test_labels)))))
+                      lapply(1:length(sl_overlap), function(x) removeGenes(x, dat, test_labels)))))
 
 # change NAs to zero
 acc_remove[is.na(acc_remove)] <- 0
 
+acc_remove %>% write_csv("data/05_performance/human_missing_train.csv")
 # PLOT
 ggplot(acc_remove, aes(y=acc, x=factor(ngenes)))+geom_boxplot()+ylab("accuracy (50 runs)")+
   ylim(0,1.0)+xlab("number of genes removed")+
   ggtitle("Sensitivity to gene dropout")
+ggsave(sprintf("figures/dropout_sens_%s_train.png", organism), dpi="print", width=6, height=4)
+
+# look at the test data
+list.test_c <- lapply(0:2, function(i) readInTest(organism, i, "common", common.genes)) 
+test_rank <- do.call(cbind, lapply(list.test_c, function(x) x$rank))
+test_lab <- unlist(sapply(list.test_c, function(x) x$lab))
+
+test_sl <- test_rank[sl_overlap,] # there are 31 total
+nas.by.col <- apply(test_sl, 2, function(x) sum(is.na(x)))
+summary(nas.by.col)
+table(nas.by.col<=1) # 4510 <-- we can try these!
+
+test_rank2 <- test_sl[,c(nas.by.col<=1)]
+test_lab2 <-test_lab[c(nas.by.col<=1)]
+
+preds.1na.test <- predSexLab(fit, as.matrix(test_rank2))
+calcAcc(test_lab2, preds.1na.test) # 95.3%
+
+acc_remove_test <- do.call(rbind, lapply(1:50, function(i)
+  do.call(rbind, 
+          lapply(1:length(sl_overlap), function(x) removeGenes(x, test_rank2, test_lab2)))))
+
+# change NAs to zero
+acc_remove_test[is.na(acc_remove_test)] <- 0
+acc_remove_test %>% write_csv("data/05_performance/human_missing_test.csv")
+
+# PLOT
+ggplot(acc_remove_test, aes(y=acc, x=factor(ngenes)))+geom_boxplot()+ylab("accuracy (50 runs)")+
+  ylim(0,1.0)+xlab("number of genes removed")+
+  ggtitle("Sensitivity to gene dropout")
+ggsave(sprintf("figures/dropout_sens_%s_test.png", organism), dpi="print", width=6, height=4)
 
 
-
-# // TODO: accuracy vs number of genes missing for the test data?
-
+# // TODO: 
+# - accuracy vs number of genes missing for the test data?
+# - gene importance
 
